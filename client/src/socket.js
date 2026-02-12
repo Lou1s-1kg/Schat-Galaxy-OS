c8fa5dce6c658c99843c1e18493e4014b42c8e8e
Lou1s-1kg <161433249+Lou1s-1kg@users.noreply.github.com>
2026-02-12 13:42:50 +0800

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,12 +2,13 @@ import io from 'socket.io-client';
 
 // 自动判断环境
 const URL = process.env.NODE_ENV === 'production' 
-  ? 'https://schat-server-louis.onrender.com' // 👈 这里填你刚才在 Render 复制的真实网址
+  ? 'https://schat-server-louis.onrender.com' 
   : 'http://localhost:3001';
 
 const socket = io(URL, {
   transports: ['websocket'],
   secure: true, // 开启安全模式
 });
+
 
 export default socket;
